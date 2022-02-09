5b23c6f168277da4988ba3488e9faedc9902382d
Antoine Desjardins <antoine.desjardins@eleves.enpc.fr>
2022-02-09 17:21:16 +0100

    generateur marche

--- projet3/generateur.jl
+++ projet3/generateur.jl
@@ -1,25 +1,41 @@
-# generateur d'Instances pour la RODD
+# generateur d'Instances pour la RODD projet 3
 
 
-n = 10
-m = 10
+N = 8
+Nm = 4
+Nf = 4
+C = 1
+G = 5
+A = 2
+T = 50
+init = 0.001
 
 
 
 # Ouvrir le fichier "output.txt" dans lequel on pourra écrire
-fout = open("outputn10m10.txt", "w")
+fout = open("output.txt", "w")
+
+println(fout, "title")
+println(fout, "title")
+println(fout, "title")
 println(fout, "title")
 
-println(fout, "n = " * string(n))
-println(fout, "m = " * string(m))
+println(fout, "N = " * string(N))
+println(fout, "Nm = " * string(Nm))
+println(fout, "Nf = " * string(Nf))
+println(fout, "C = " * string(C))
+println(fout, "G = " * string(G))
+println(fout, "A = " * string(A))
+println(fout, "T = " * string(T))
+println(fout, "init = " * string(init))
 
-for i in 1:n
-  myLine = ""
-  for j in 1:(m-1)
-    myLine = myLine * string(rand(1:10)) * " "
+for i in 1:N
+  for j in 1:G
+    myLine = string(i) * " " * string(1) * " " * string(j) * " " * string(1) * " " * string(rand(1:A))
+    println(fout, myLine)
+    myLine = string(i) * " " * string(1) * " " * string(j) * " " * string(2) * " " * string(rand(1:A))
+    println(fout, myLine)
   end
-  myLine = myLine * string(rand(1:10))
-  println(fout, myLine)
 end
 
 close(fout)
