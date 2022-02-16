d8725bca60e48626c9e967978165252b3a5c6d5c
Antoine Desjardins <antoine.desjardins@eleves.enpc.fr>
2022-02-16 15:00:39 +0100

    projet 4 parser et generateur

---
+++ projet4/generateur.jl
@@ -0,0 +1,34 @@
+# generateur d'Instances pour la RODD projet 4
+
+
+P = 1
+m = 10
+n = 10
+w1 = 1
+w2 = 5
+L = 3
+g = 1.26157
+
+
+
+# Ouvrir le fichier "output.txt" dans lequel on pourra écrire
+fout = open("output.txt", "w")
+
+println(fout, "P = " * string(P))
+println(fout, "m = " * string(m))
+println(fout, "n = " * string(n))
+println(fout, "w1 = " * string(w1))
+println(fout, "w2 = " * string(w2))
+println(fout, "L = " * string(L))
+println(fout, "g = " * string(g))
+println(fout, "title")
+
+for i in 1:n
+  myLine = string(i)
+  for j in 1:(m)
+    myLine = myLine * " " * string(rand(70:100))
+  end
+  println(fout, myLine)
+end
+
+close(fout)
--- projet4/parser.jl
+++ projet4/parser.jl
@@ -6,56 +6,25 @@ function read_instance(MyFileName::String)
   if isfile(path)
     # L’ouvrir
     myFile = open(path)
+
+    P = parse(Int64, split(readline(myFile), " ")[3])
+    m = parse(Int64, split(readline(myFile), " ")[3])
+    n = parse(Int64, split(readline(myFile), " ")[3])
+    w1 = parse(Int64, split(readline(myFile), " ")[3])
+    w2 = parse(Int64, split(readline(myFile), " ")[3])
+    L = parse(Int64, split(readline(myFile), " ")[3])
+    g = parse(Float64, split(readline(myFile), " ")[3])
+
     readline(myFile)
 
-    n = parse(Int64, split(readline(myFile), " ")[3])
-    P = parse(Int64, split(readline(myFile), " ")[3])
-
-    cout = Array{Int64,2}(ones(n,n)) # on met a zero pour l'instant
+    t = Array{Int64,2}(ones(n,m)) # on met a zero pour l'instant
     for i in 1:n
-      for j in 1:n
-        line =  split(readline(myFile), " ")
-        cout[parse(Int64,line[1]),parse(Int64,line[2])] = parse(Int64,line[3])
+    line =  split(readline(myFile), " ")
+      for j in 1:m
+      t[i,j] = parse(Int64,line[j + 1])
       end
     end
 
-    alpha1 = Array{Float64,1}(zeros(P))
-    for i in 1:P
-      alpha1[i] = 0.5 # donnees du poly
-    end
-    alpha2 = Array{Float64,1}(zeros(P))
-    for i in 1:3
-      alpha2[i] = 0.9 # donnees du poly
-    end
-    for i in 4:6
-      alpha2[i] = 0.5 # donnees du poly
-    end
-    alpha3 = Array{Float64,1}(zeros(P))
-    for i in 1:3
-      alpha3[i] = 0.5 # donnees du poly
-    end
-    for i in 4:6
-      alpha3[i] = 0.9 # donnees du poly
-    end
-    alpha4 = Array{Float64,1}(zeros(P))
-    for i in 1:3
-      alpha4[i] = 0.8 # donnees du poly
-    end
-    for i in 4:6
-      alpha4[i] = 0.6 # donnees du poly
-    end
-
-    indice_danger = Array{Float64,1}(zeros(P))
-    for i in 1:3
-      indice_danger[i] = 1
-    end
-
-    Survie = Array{Float64,3}(zeros(P,n,n))
-    data = readlines(myFile)
-		for datum in data
-      line = split(datum, " ")
-      Survie[parse(Int64,line[1]),parse(Int64,line[2]),parse(Int64,line[3])] = parse(Float64,line[4])
-    end
-    return n, P, alpha4, Survie, indice_danger, cout
+    return P, m, n, w1, w2, L, g, t
   end
 end
