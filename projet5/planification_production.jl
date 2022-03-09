6b3ab52e6c0af57a9842bfdcb0dd19e4b76d25d7
Antoine Desjardins <antoine.desjardins@eleves.enpc.fr>
2022-03-09 17:31:26 +0100

    plot

--- projet5/planification_production.jl
+++ projet5/planification_production.jl
@@ -1,11 +1,11 @@
 using JuMP
 using CPLEX
-
+using Plots
 
 function Rolling(Tprim::Int64)
-  T, M, Emax = 12, 4, 3
-  f = [10,30,60,90]
-  e = [8,6,4,2]
+  T, M, Emax = 12, 6, 3
+  f = [10,30,60,90, 10, 30]
+  e = [8,6,4,2, 8, 6]
   h = Array{Int64,1}(ones(T))
   p = Array{Int64,2}(zeros(T,M))
   d = Array{Int64,1}(ones(T))
@@ -37,15 +37,29 @@ function Rolling(Tprim::Int64)
   cout_carbone = sum(e[m]*x_sol[t,m] for m in 1:M, t in 1:T)
   println(fout, "cout carbone = ", cout_carbone)
   println(fout, "objective value = ", JuMP.getobjectivevalue.(model))
+  return cout_carbone, JuMP.getobjectivevalue.(model)
 end
 
-fout = open("resultats.txt", "w")
+fout = open("resultatsEmax3M6.txt", "w")
 
+T = 12
+cout_carbone_history = Array{Float64,1}(zeros(T))
+cout_history = Array{Float64,1}(zeros(T))
 
-for Tprim in 1:12
+for Tprim in 1:T
   start = time()
   println(fout, "Tprim = ", Tprim)
-  Rolling(Tprim)
+  cout_carbone, cout = Rolling(Tprim)
+  cout_carbone_history[Tprim] = cout_carbone
+  cout_history[Tprim] = cout
   println(fout, "execution time = ", time() - start, "s")
 end
 close(fout)
+
+plot(1:T,cout_carbone_history)
+xlabel!("Tprim")
+ylabel!("cout carbone")
+
+plot(1:T,cout_history)
+xlabel!("Tprim")
+ylabel!("cout")
