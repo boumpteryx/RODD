ba69ece323a59a76415d06562a4784b2aa5bdf99
Antoine Desjardins <antoine.desjardins@eleves.enpc.fr>
2022-03-09 17:11:16 +0100

    finished

--- projet5/planification_production.jl
+++ projet5/planification_production.jl
@@ -5,7 +5,7 @@ using CPLEX
 start = time()
 
 function Rolling()
-  T, M, Emax = 12, 4, 3
+  T, M, Emax, Tprim = 12, 4, 3, 2
   f = [10,30,60,90]
   e = [8,6,4,2]
   h = Array{Int64,1}(ones(T))
@@ -16,25 +16,31 @@ function Rolling()
   end
 
   # Create the model
-  m = Model(CPLEX.Optimizer)
+  model = Model(CPLEX.Optimizer)
 
   ## Variables
-  @variable(m, x[], Bin)
-  @variable(m, y[] >= 0)
+  @variable(model, x[1:T,1:M] >= 0)
+  @variable(model, y[1:T,1:M], Bin)
+  @variable(model, s[1:T+1] >= 0)
 
   ## Constraints
-  @constraint(m, )
+  @constraint(model, s[1] == 0)
+  @constraint(model, [t in 2:T+1], sum(x[t-1,m] - s[t] + s[t-1] for m in 1:M) == d[t-1])
+  @constraint(model, [t in 1:T, m in 1:M], x[t,m] <= y[t,m]*sum(d[tprim] for tprim in t:T))
+  @constraint(model, [t in 1:(T-Tprim)], sum((e[m] - Emax)*x[tprim,m] for tprim in t:(t+Tprim), m in 1:M) <= 0)
 
   ## Objective
-  @objective(m, Max, )
+  @objective(model, Min, sum(f[m]*y[t,m] for m in 1:M, t in 1:T) + sum(s[t] for t in  1:T+1))
 
   #resolution
-  optimize!(m)
+  optimize!(model)
 
-  # solution_summary(m, verbose=true)
-  # z_vals = JuMP.getvalue.( m[:z] )
-  # println(z_vals)
+  x_sol = JuMP.getvalue.(model[:x])
+  cout_carbone = sum(e[m]*x_sol[t,m] for m in 1:M, t in 1:T)
+  println("cout carbone = ", cout_carbone)
+  println("objective value = ", JuMP.getobjectivevalue.(model))
 
 end
 
 Rolling()
+println("execution time = ", time() - start, "s")
